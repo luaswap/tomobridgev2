69e0ecfea38afa1677625fdc99eef9d6260febc0
pqv199x <pqv199x@hotmail.com>
2021-06-17 15:09:52 +0700

    check claim time

--- app/Select.vue
+++ app/Select.vue
@@ -170,6 +170,7 @@ export default {
             const data1 = await this.checkUnclaimTx()
             if (data1) {
                 this.$store.state.unClaimTx = data1
+                this.$bus.$emit('reclaim')
                 this.$refs.claimModal.show()
             }
         }
