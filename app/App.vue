895726f76500abc2fc46fa63d36133797444f370
pqv199x <pqv199x@hotmail.com>
2021-06-23 16:00:11 +0700

    add coming soon

--- app/App.vue
+++ app/App.vue
@@ -23,7 +23,7 @@
                         <b-nav-item
                             active
                             to="/select">Home</b-nav-item>
-                        <b-nav-item>Instruction video</b-nav-item>
+                        <b-nav-item class="position-relative">Instruction video <span class="coming-soon">Coming soon</span></b-nav-item>
                         <b-nav-item
                             href="https://forms.gle/cU1XU3b8EUMxB6yA6"
                             target="_blank">Support ticket</b-nav-item>
