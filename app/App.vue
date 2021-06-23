1c2f6482096241b6105162353ad10602406dd200
pqv199x <pqv199x@hotmail.com>
2021-06-23 09:43:10 +0700

    update links

--- app/App.vue
+++ app/App.vue
@@ -18,67 +18,6 @@
                 <b-collapse
                     id="nav-collapse"
                     is-nav>
-                    <div
-                        class="ml-auto navbar-buttons d-none flex align-content-center">
-                        <b-navbar-nav>
-                            <b-nav-item-dropdown
-                                v-if="address"
-                                class="info-wallet btn-token-dropdown"
-                                offset="25"
-                                no-caret
-                                right>
-                                <template
-                                    slot="button-content"
-                                    class="tmp-btn-transparent">
-                                    <i class="tb-wallet"/>
-                                    {{ truncate(address, 16) }}
-                                </template>
-                                <b-dropdown-item
-                                    class="flex_box">
-                                    <span>Balance:</span>
-                                    <strong>{{ balance }} {{ tomoIds.indexOf(network.chainId || '') > -1 ? 'TOMO' : 'ETH' }}</strong>
-                                </b-dropdown-item>
-                                <b-dropdown-item
-                                    :href="scanUrl"
-                                    class="flex_box"
-                                    target="_blank">
-                                    Transaction History
-                                </b-dropdown-item>
-                                <b-dropdown-divider/>
-                                <b-dropdown-item
-                                    v-if="!mobileCheck"
-                                    class="sign_out"
-                                    @click="signOut">
-                                    Sign out
-                                </b-dropdown-item>
-                            </b-nav-item-dropdown>
-                        </b-navbar-nav>
-                        <div
-                            v-if="address"
-                            id="networkDiv"
-                            class="btn-token network ml-3">{{ network.name }}</div>
-                        <!-- <b-tooltip
-                            v-if="address"
-                            target="networkDiv">
-                            <a
-                                href="https://docs.tomochain.com/general/how-to-connect-to-tomochain-network/metamask"
-                                target="_blank"><u>Switch Networks</u></a>
-                            between Ethereum & TomoChain to wrap and unwrap tokens
-                        </b-tooltip> -->
-                        <b-dropdown
-                            :text="selectedLanguage"
-                            class="nav-item btn-language ml-2">
-                            <b-dropdown-item
-                                class="current-lang"
-                                @click="changeLang('english')">English</b-dropdown-item>
-                            <!-- <b-dropdown-item
-                                @click="changeLang('turkish')">Türk</b-dropdown-item>
-                            <b-dropdown-item
-                                @click="changeLang('japanese')">日本語</b-dropdown-item>
-                            <b-dropdown-item
-                                @click="changeLang('chinese')">简体中文</b-dropdown-item> -->
-                        </b-dropdown>
-                    </div>
                     <b-nav
                         class="text-white ml-auto">
                         <b-nav-item
@@ -86,10 +25,10 @@
                             to="/select">Home</b-nav-item>
                         <b-nav-item>Instruction video</b-nav-item>
                         <b-nav-item
-                            to="https://forms.gle/cU1XU3b8EUMxB6yA6"
+                            href="https://forms.gle/cU1XU3b8EUMxB6yA6"
                             target="_blank">Support ticket</b-nav-item>
                         <b-nav-item
-                            to="https://docs.tomochain.com/tomobridge/faq"
+                            href="https://docs.tomochain.com/tomobridge/faq"
                             target="_blank">FAQ</b-nav-item>
                         <b-dropdown
                             :text="selectedLanguage"
