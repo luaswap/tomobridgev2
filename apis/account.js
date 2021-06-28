85a1338bc0bf9c9b133ba636e8c4144eb987f7dd
pqv199x <pqv199x@hotmail.com>
2021-06-28 15:52:07 +0700

    add updatetxwithkey api

--- apis/account.js
+++ apis/account.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator/check')
 const db = require('../models/mongodb')
 const Web3 = require('web3')
+const config = require('config')
 
 router.get('/getUnclaimTx/:address', [], async function (req, res, next) {
     const errors = validationResult(req)
@@ -62,11 +63,58 @@ router.post('/updateTx', [
                 signer: address.toLowerCase(),
                 coin: coin.toLowerCase(),
                 burnTx: burnTx
-            }, data, { upsert: true })
+            }, { $set: data }, { upsert: true })
         }
         return res.send('OK')
     } catch (error) {
         return res.send('Cannot find tx')
+    }
+})
+
+router.post('/updateTxWithKey', [
+    check('address').exists().isLength({ min: 42, max: 42 }).withMessage("'address' is incorrect."),
+    check('burnTx').exists().withMessage("'burnTx' is required.").isLength({ min: 66, max: 66 }).withMessage("'burnTx' is incorrect."),
+    check('claimTx').optional().isLength({ min: 66, max: 66 }).withMessage("'claimTx' is incorrect."),
+    check('isClaim').exists().isBoolean().withMessage("'isClaim' is required.")
+], async function (req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return next(errors.array())
+    }
+    try {
+        if (!req.headers.authorization || req.headers.authorization !== config.get('apiKey')) {
+            return next(new Error('Not Acceptable'))
+        } else {
+            const address = req.body.address
+            const burnTx = req.body.burnTx
+            const claimTx = req.body.claimTx || ''
+            const isClaim = req.body.isClaim
+            let data = await db.Transaction.findOne({
+                signer: address.toLowerCase(),
+                burnTx: burnTx
+            })
+            if (!data) {
+                return next(new Error('Tx not found'))
+            } else {
+                const web3 = new Web3()
+                if (!web3.utils.isAddress(address)) {
+                    return next(new Error('address is invalid'))
+                } else {
+                    await db.Transaction.updateOne({
+                        signer: address.toLowerCase(),
+                        burnTx: burnTx
+                    }, {
+                        $set: {
+                            claimTx,
+                            isClaim
+                        }
+                    }, { upsert: true })
+                }
+            }
+            return res.send('OK')
+        }
+    } catch (error) {
+        return next(error)
     }
 })
 
