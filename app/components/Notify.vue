0f28ed1bcc2c7b6fcc7435c1de61b9e63f8c4caf
Theo Dao <trungdao912@gmail.com>
2022-09-16 11:16:35 +0700

    fix: update text for notify component

--- app/components/Notify.vue
+++ app/components/Notify.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="row infor-wallet mb-4 txt-red">
     <div class="col-12">
-    <div>
-      For a better user experience, we have moved the following tokens to Multichain Bridge: ETH
-    </div>
-    <div>
-      Proceed to <a :href="multichainUrl" target="_blank" class="text-blue">
-        Multichain
-      </a> to continue wrapping/unwrapping tokens above. For any further assistance, <a :href="multichainSupportUrl"
-        target="_blank" class="text-blue">please contact Multichain for
-        support.</a>
-    </div>
+      <div>
+        For a better user experience, we have moved the following tokens to Multichain Bridge: ETH, USDT, LUA, SRM, FTT
+      </div>
+      <div>
+        Proceed to <a :href="multichainUrl" target="_blank" class="text-blue">
+          Multichain
+        </a> to continue wrapping/unwrapping tokens above. For any further assistance, <a :href="multichainSupportUrl"
+          target="_blank" class="text-blue">please contact Multichain for
+          support.</a>
+      </div>
     </div>
 
   </div>
@@ -24,6 +24,6 @@ export default {
       multichainUrl: 'https://app.multichain.org/#/router',
       multichainSupportUrl: 'https://multichain.zendesk.com/hc/en-us'
     }
-  },
+  }
 }
 </script>
